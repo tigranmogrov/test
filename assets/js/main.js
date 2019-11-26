405b04b16a7f6e602a7826826077720831906126
Mogrovyan Tigran <tigranmogrov@gmail.com>
2019-11-26 18:59:57 +0200

    add slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,71 @@ $(document).ready(function () {
   
   });
 
+  $('.about-carusel').slick({
+    infinite: false,
+    speed: 900,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow: '<i class="fas fa-chevron-left"></i>',
+    nextArrow: '<i class="fas fa-chevron-right"></i>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 
+  $('.clients-content').slick({
+    infinite: false,
+    speed: 900,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 document.body.onload = function () {
     setTimeout(function () {
         var preloader = document.getElementById("preloader");
