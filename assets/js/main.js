86aa57702d3f8c0b7505cd775d4b99ee950f72c6
Mogrovyan Tigran <tigranmogrov@gmail.com>
2019-11-27 16:16:00 +0200

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,31 +46,7 @@ $(document).ready(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
-    dots: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+    dots: true
   });
 document.body.onload = function () {
     setTimeout(function () {
